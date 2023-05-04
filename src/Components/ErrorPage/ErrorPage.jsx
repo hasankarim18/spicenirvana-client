140acae4c5047277b79760100fa19065c7c2b60c
hasankarim18 <hallysust@gmail.com>
2023-05-04 10:33:47 +0600

    footer added

--- src/Components/ErrorPage/ErrorPage.jsx
+++ src/Components/ErrorPage/ErrorPage.jsx
@@ -1,10 +1,27 @@
+import { FaSadTear } from "react-icons/fa";
+import { Link, useRouteError } from "react-router-dom";
 
 
 const ErrorPage = () => {
+
+    const error = useRouteError();
+
     return (
-        <div className="mx-auto h-screen w-screen p-20 flex justify-center items-center ">
-            Error page 
+      <div className="mx-auto h-screen w-screen p-20 flex justify-center items-center ">
+        <div>
+          <div style={{ fontSize: "150px" }} className="text-green-500 flex justify-center">
+            <FaSadTear />
+          </div>
+          <div className="text-center">
+            <h1>Oops!</h1>
+            <p>Sorry, an unexpected error has occurred.</p>
+            <p>
+              <i>{error.statusText || error.message}</i>
+            </p>
+            <Link to="/" className="btn w-full mt-4" > Back to Home </Link>
+          </div>
         </div>
+      </div>
     );
 };
 
--- src/Shared/Footer/Footer.jsx
+++ src/Shared/Footer/Footer.jsx
@@ -1,11 +1,56 @@
+import { FaFacebook,  FaInstagramSquare, FaTwitterSquare } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 
 const Footer = () => {
     return (
-      <div>
-        <div className="container mx-auto">
+      <div className="mt-20 bg-black text-white  bg-opacity-90 py-12">
+        <div className="container mobile_padding mx-auto">
           {" "}
-          <h1>Footer</h1>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+            <div>
+              <h1 className="text-3xl">SpiceNirvana</h1>
+              <p className="text-orange-400">
+                Unleash your inner chef with our recipes.
+              </p>
+              <p>
+                Cooking made easy with our recipe website! From quick weeknight
+                meals to fancy feasts, we haveve got recipes that will make your
+                taste buds dance. So whether you are a kitchen novice or a
+                seasoned pro, our recipes are sure to impress. Do not believe
+                us? Try them out for yourself and get ready to be wowed!
+              </p>
+            </div>
+            <div>
+              <h2 className="text-xl mb-4">Contact Us</h2>
+              <p className="mb-4">Email: admin@spicenirvana.com </p>
+              <p className="mb-4">Mobile: +880101010101 </p>
+              <p>
+                House #123, Road #ABC, Block DEF Gulshan 2, Dhaka 1206
+                Bangladesh
+              </p>
+            </div>
+            <div>
+              <h2 className="text-xl mb-4">Usefull Links</h2>
+              <ul>
+                <li className="mb-4">
+                  <Link className="hover:text-orange-400" to="/user-profile">
+                    User Profile
+                  </Link>
+                </li>
+                <li className="mb-4">
+                  <Link className="hover:text-orange-400" to="/blog">
+                    Blog
+                  </Link>
+                </li>
+              </ul>
+              <ul className="flex gap-4" >
+                <li> <a className="text-3xl hover:text-orange-400" href="#">  <FaFacebook /> </a></li>
+                <li> <a className="text-3xl hover:text-orange-400" href="#">  <FaTwitterSquare /> </a></li>
+                <li> <a className="text-3xl hover:text-orange-400" href="#">  <FaInstagramSquare /> </a></li>
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
     );
